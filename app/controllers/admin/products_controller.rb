13c9a6770ac055bdab2acd2e0898e200b18249f4
Sean Wonders <wonders.sean@gmail.com>
2016-03-04 17:16:35 +0800

    require is_admin to browse products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,8 @@
 class Admin::ProductsController < ApplicationController
 
-	before_action :anthenticate_user!
-	
+	before_action :authenticate_user!
+	before_action :admin_required
+
 	def index
 		@products = Product.all
 	end

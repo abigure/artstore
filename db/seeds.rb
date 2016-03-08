31ab530efa8c00d9563bf9141622f66653eaefee
Sean Wonders <wonders.sean@gmail.com>
2016-03-08 17:24:43 +0800

    use seed to create admin user with is_admin = true

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(	email: "admin@test.com", 
+							password: "12345678", 
+							password_confirmation: "12345678", 
+							is_admin: true)
